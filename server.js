ae32814f5502dd84fca16d65a6172c6def4f881a
indrayanikalake <indraayanikalaake333@gmail.com>
2023-09-17 17:37:46 +0530

    writing and readin from a file having issues

--- server.js
+++ server.js
@@ -1,36 +1,63 @@
 const http = require('http');
+const fs = require('fs');
 
-let port = 5000;
+const port = 5000;
 
-const server = http.createServer((req,res)=>{
-   
-if(req.url === '/home'){
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<header><title>Home page</title></header>')
-    res.write('<body><h1>Hello User Welcom to our website</h1></body>')
-    res.write('</html>')
-  
-    res.end()
-  }else if(req.url === '/about'){
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<header><title>about page</title></header>')
-    res.write('<body><h1>Hello User Welcom to our about page</h1></body>')
-    res.write('</html>')
-  
-    res.end()
-  }else if(req.url === '/node'){
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<header><title>node page</title></header>')
-    res.write('<body><h1>Hello User Welcom to our nodejs website</h1></body>')
-    res.write('</html>')
-  
-    res.end()
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('message.txt', 'utf-8', (error, data) => {
+      if (error) {
+        console.error(error);
+        res.statusCode = 500; // Internal Server Error
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+        return;
+      }
+
+      console.log(data);
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<body><title>Home Page</title></body>');
+      res.write(
+        `<body><h1>${data}</h1><form method="POST" action="/message">
+          <input name="message" type="text" />
+          <button type="submit">Submit</button>
+        </form></body>`
+      );
+      res.write('</html>');
+      res.end();
+    });
+
+    return;
   }
-})
 
-server.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-})
+  if (req.url === '/message' && req.method === 'POST') {
+    const body = [];
+    req.on('data', (chunkOfData) => {
+      body.push(chunkOfData);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = decodeURIComponent(parsedBody.replace(/\+/g, ' ')).replace('message=', '');
+
+      fs.writeFile('message.txt', message, (error) => {
+        if (error) {
+          console.error(error);
+          res.statusCode = 500; // Internal Server Error
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('Internal Server Error');
+          return;
+        }
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+      });
+    });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
